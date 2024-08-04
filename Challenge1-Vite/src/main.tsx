31a80d61511071a8f9b4d7970e2b63a2acd392f2
Santiago Palacio <santiagopalacio@192.168.1.8>
2024-08-04 17:33:14 -0300

    feat: debounce and search through endpoint

--- Challenge1-Vite/src/main.tsx
+++ Challenge1-Vite/src/main.tsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </QueryClientProvider>
 );
